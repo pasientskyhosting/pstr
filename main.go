f61d207726e53a03ac8bec90dfefc0e5e71c1607
Andreas Kruger <ak@pasientsky.no>
2017-06-20 09:01:06 +0200

    Fixed default value for TargetCPUUtilizationPercentage when not set

--- main.go
+++ main.go
@@ -167,6 +167,12 @@ func main() {
 				if M_GERNICSERVICE {
 					createGenericService(AppObj)
 				}
+
+                // Set a default
+                if AppObj.Scalability.TargetCPUUtilizationPercentage = "" || AppObj.Scalability.TargetCPUUtilizationPercentage < 10 {
+                    AppObj.Scalability.TargetCPUUtilizationPercentage = 70
+                }
+
 				if M_AUTOSCALER {
 					if AppObj.Resources.Requests.Cpu != "" && AppObj.Scalability.MinReplicas > 0 && AppObj.Scalability.MaxReplicas > 1 && AppObj.Scalability.TargetCPUUtilizationPercentage > 0 {
 						createAutoScaler(AppObj)
